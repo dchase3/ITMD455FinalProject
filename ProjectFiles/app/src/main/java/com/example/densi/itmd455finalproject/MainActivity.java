dbc3b55e1abc1d4d37183cef45a36dfda6edad87
Dennis Chase <dchase3@hawk.iit.edu>
2018-04-05 15:41:47 -0500

    Added skeleton files

---
+++ ProjectFiles/app/src/main/java/com/example/densi/itmd455finalproject/MainActivity.java
@@ -0,0 +1,15 @@
+//first package will be different on each computer make use you add yours
+//and comment out the others on your computer.
+//package com.example.densi.itmd455finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
