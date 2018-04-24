85d65360c08e68c34f59dc5ef9866ec1daca5d98
Dennis Chase <dchase3@hawk.iit.edu>
2018-04-23 23:53:18 -0500

    Added Dev's files

--- ProjectFiles/app/src/main/java/com/example/densi/itmd455finalproject/MainActivity.java
+++ ProjectFiles/app/src/main/java/com/example/densi/itmd455finalproject/MainActivity.java
@@ -1,15 +1,62 @@
 //first package will be different on each computer make use you add yours
 //and comment out the others on your computer.
-//package com.example.densi.itmd455finalproject;
+package com.example.densi.itmd455finalproject;
+//package com.example.ssiruuk.itmd455finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.editText);
+        password = (EditText)findViewById(R.id.editText2);
+        submit = findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String str = username.getText().toString();
+                Intent i = new Intent(MainActivity.this, display.class);
+                i.putExtra("Username", str);
+                startActivity(i);
+            }
+        });
+    }
+    /*
+        @Override
+        public void onClick(View view) {
+            if (validate()){
+                Toast.makeText(MainActivity.this, "Success..!", Toast.LENGTH_LONG)
+                        .show();
+                /*String str = username.getText().toString();
+                Intent i = new Intent(MainActivity.this, display.class);
+                i.putExtra("Username", str);
+                startActivity(i);
+            }
+        }
+    */
+    public boolean validate() {
+        //simple test to see if fields were populated
+        if (username.getText().toString().trim().length()<=0){
+            Toast.makeText(MainActivity.this, "Please Enter a valid username",
+                    Toast.LENGTH_LONG).show();
+            return true;
+        } else if (password.getText().toString().trim().length()<=0){
+            Toast.makeText(MainActivity.this, "Please enter a valid password",
+                    Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
     }
 }
